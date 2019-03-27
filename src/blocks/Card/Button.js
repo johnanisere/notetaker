4d71799e0dc1404bcf799d594f8c49a19b4ef30b
John Anisere <johnanisere@gmail.com>
2019-03-27 23:58:06 +0100

    implement responsiveness

--- src/blocks/Card/Button.js
+++ src/blocks/Card/Button.js
@@ -4,7 +4,7 @@ import Button from "../../elements/Button";
 
 export default styled(Button)`
   padding: 0px 12px;
-  background-color: #f1f1f1;
+  background-color: rgba(255, 255, 255, 0.4);
   border-radius: 5px;
   box-shadow: 1px 1px 1px 1px #ffffff;
   border: 1px solid rgba(0, 0, 0, 0.05);
@@ -13,4 +13,10 @@ export default styled(Button)`
   padding: 5px 8px;
   cursor: pointer;
   color: #888888;
+  transition: all 0.1s ease-in-out;
+  transform: scale(0.98);
+  &:hover {
+    box-shadow: 1px 1px 2px 3px #ffffff;
+    transform: scale(1);
+  }
 `;
